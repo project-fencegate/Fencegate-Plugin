4739ba38e7b9a4908e72298352cfedbc924fccfb
berry <spamnut@yeah.net>
2019-01-28 20:14:11 +0800

    hi

--- src/main/java/cc/fencegate/plugin/Fencegate.java
+++ src/main/java/cc/fencegate/plugin/Fencegate.java
@@ -1,12 +1,38 @@
 package cc.fencegate.plugin;
 
 import cc.fencegate.plugin.commands.MainCommand;
+import cc.fencegate.plugin.storage.SQLiteStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class Fencegate extends JavaPlugin {
+    public static Fencegate instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         Bukkit.getPluginCommand("fencegate").setExecutor(new MainCommand());
+
+
+    }
+
+    private void sync_UserData(){
+
+    }
+
+    private  void loadConfig(){
+        getDataFolder().mkdirs();
+        File f = new File(getDataFolder(), "config.yml");
+        if(!f.exists())
+            saveDefaultConfig();
+        reloadConfig();
+
+        f = new File(getDataFolder()+File.separator+"data"); //plugins storage
+        if(!f.exists())
+            f.mkdirs();
+
+        new SQLiteStorage("data");
     }
 }

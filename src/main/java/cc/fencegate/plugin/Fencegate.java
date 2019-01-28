6d8b9141f53e025296f6ce85cd2308b8b4fe2f7d
berry <spamnut@yeah.net>
2019-01-28 12:13:22 +0800

    Basic Commands

--- src/main/java/cc/fencegate/plugin/Fencegate.java
+++ src/main/java/cc/fencegate/plugin/Fencegate.java
@@ -1,10 +1,12 @@
 package cc.fencegate.plugin;
 
+import cc.fencegate.plugin.commands.MainCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Fencegate extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        Bukkit.getPluginCommand("fencegate").setExecutor(new MainCommand());
     }
 }

84b79cbb99ea1b4f1c9f48c81efc1d3646500aee
berry <spamnut@yeah.net>
2019-01-29 09:55:21 +0800

    logic update

--- src/main/java/cc/fencegate/plugin/Fencegate.java
+++ src/main/java/cc/fencegate/plugin/Fencegate.java
@@ -20,6 +20,34 @@ public class Fencegate extends JavaPlugin {
 
     private void sync_UserData(){
 
+        /*
+        Sample data request JSON
+        {
+          "username": "example@fencegate.cc",
+          "uid": "key(different per-server maybe?)",
+          "plugins": {
+            "plugin1-id": "plugin1 md-5 value",
+            "XXXX-XXXX-XXXX-XXXX p2 uuid": "plugin2 md-5 value"
+          }
+        }
+
+
+        data return JSON
+        {
+          "uid": "if uid doesnt match stop",
+          "plugins": {
+            "plugin1-id": "secret key",
+            "plugin2-id": "secret key",
+            "plugin3-id": null <- doesnt load plugin3
+          }
+        }
+
+        the secret key will be used to "unlock" the plugin(all plugins that extend
+        SecurePlugin base will be locked and will require an unlock to work.
+
+        unlocking sends the server-id, plugin-uid, md-5, and secret key to server,
+        server returns a value that allows the plugin's onEnable() to be called.
+         */
     }
 
     private  void loadConfig(){
